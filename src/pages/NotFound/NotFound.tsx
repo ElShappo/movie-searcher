bfaf6340199ecb37248582888cf95be957f16ab6
Deni Shaipov <deni.shaipp@gmail.com>
2024-04-11 15:46:13 +0300

    improve notFoundPage look

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
-    <div className="text-3xl max-md:text-base w-full h-screen flex justify-center items-center">
-      <div className="rounded-xl bg-blue-800 p-10 max-md:p-5">404 (page not found)</div>
+    <div className="absolute top-0 bottom-0 left-0 right-0 text-2xl max-md:text-xl -z-10">
+      <div className="h-full flex justify-center items-center text-center px-4">
+        <p className="rounded-xl bg-blue-800 p-5">404 (page not found)</p>
+      </div>
     </div>
   );
 };
